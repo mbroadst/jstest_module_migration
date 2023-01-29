5ab94248c32b544a4141c2366135b6b46851864a
Matt Broadstone <mbroadst@mongodb.com>
2023-01-29 13:11:32 +0000

    exclude overrides when listing tests/libs

--- scripts/list_libs.sh
+++ scripts/list_libs.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-# List all files which are loaded from other jstest files (we call them "libs")
+# List all files which are loaded from other jstest files (we call them "libs"). Excludes overrides.
 
 SEARCH_PATH=${1:-jstests}
-grep -ho 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH/**/*.js | cut -c 7- | sort | uniq
+TMP_FILE=$(mktemp /tmp/list_libs.XXXXXX)
+
+# find all paths that show up in `load` calls
+grep -ho 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH/**/*.js | grep -ve "override" | cut -c 7- | sort | uniq > $TMP_FILE
+# append the search path prefix
+sed -i -e "s|^|$(dirname $SEARCH_PATH)/|" $TMP_FILE
+sed -i -e "s|(.*)|$(realpath \1)|" $TMP_FILE
+
+cat $TMP_FILE
+rm $TMP_FILE
--- scripts/list_tests.sh
+++ scripts/list_tests.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# List all files which are not libs (are not loaded by other test files)
+# List all files which are not libs (are not loaded by other test files). Excludes overrides.
 
 SEARCH_PATH=${1:-jstests}
-TMP_FILE=$(mktemp /tmp/abc-script.XXXXXX)
+TMP_FILE=$(mktemp /tmp/list_tests.XXXXXX)
 
-grep -ho 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH/**/*.js | cut -c 7- | sort | uniq > $TMP_FILE
+# find all paths that are used in `load` statements, and save to a file
+grep -ho 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH/**/*.js | grep -ve "override" | cut -c 7- | sort | uniq > $TMP_FILE
+# find all files with a "js" extension that do not appear in the list of `load`ed files
 find $SEARCH_PATH -name "*.js" | grep -vFxf $TMP_FILE
+
 rm $TMP_FILE
