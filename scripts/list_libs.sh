d554f31becbd5a2e5abba74ff64aa81a94810269
Matt Broadstone <mbroadst@mongodb.com>
2023-01-29 23:17:12 +0000

    fix issue with finding `load` calls

--- scripts/list_libs.sh
+++ scripts/list_libs.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-
 # List all files which are loaded from other jstest files (we call them "libs"). Excludes overrides.
 
 SEARCH_PATH=${1:-jstests}
 TMP_FILE=$(mktemp /tmp/list_libs.XXXXXX)
 
 # find all paths that show up in `load` calls
-grep -ho 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH/**/*.js | grep -ve "override" | cut -c 7- | sort | uniq > $TMP_FILE
+grep -hro 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH | grep -vE "override|third_party" | cut -c 7- | sort | uniq | sed -e "s|^./||" > $TMP_FILE
 # append the search path prefix
 sed -i -e "s|^|$(dirname $SEARCH_PATH)/|" $TMP_FILE
 sed -i -e "s|(.*)|$(realpath \1)|" $TMP_FILE
--- scripts/list_tests.sh
+++ scripts/list_tests.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-
 # List all files which are not libs (are not loaded by other test files). Excludes overrides.
 
 SEARCH_PATH=${1:-jstests}
 TMP_FILE=$(mktemp /tmp/list_tests.XXXXXX)
 
 # find all paths that are used in `load` statements, and save to a file
-grep -ho 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH/**/*.js | grep -ve "override" | cut -c 7- | sort | uniq > $TMP_FILE
+grep -hro 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH | cut -c 7- | sort | uniq | sed -e "s|^./||" > $TMP_FILE
 # find all files with a "js" extension that do not appear in the list of `load`ed files
-find $SEARCH_PATH -name "*.js" | grep -vFxf $TMP_FILE
+find $SEARCH_PATH -type f -name "*.js" $(printf "! -path *%s* " $(cat $TMP_FILE)) | grep -vE "override|third_party"
 
 rm $TMP_FILE
