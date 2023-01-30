a677885e38e42116a0608b1cf69529c6b8bdde72
Matt Broadstone <mbroadst@mongodb.com>
2023-01-30 22:48:35 +0000

    exclude more files as libs when compiling test list

--- scripts/list_tests.sh
+++ scripts/list_tests.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 # List all files which are not libs (are not loaded by other test files). Excludes overrides.
 
+# set -x
+
 SEARCH_PATH=${1:-jstests}
 TMP_FILE=$(mktemp /tmp/list_tests.XXXXXX)
 
 # find all paths that are used in `load` statements, and save to a file
 grep -hro 'load('"'"'[^'"'"']*\|load("[^"]*' $SEARCH_PATH | cut -c 7- | sort | uniq | sed -e "s|^./||" > $TMP_FILE
+
+# add well-known libs paths
+find $SEARCH_PATH -type d -name "*libs*" | sed -e "s|^$SEARCH_PATH|jstests|" >> $TMP_FILE
+cat <<EOT >> $TMP_FILE
+jstests/hooks
+jstests/libs/override_methods
+jstests/third_party
+EOT
+
 # find all files with a "js" extension that do not appear in the list of `load`ed files
-find $SEARCH_PATH -type f -name "*.js" $(printf "! -path *%s* " $(cat $TMP_FILE)) | grep -vE "override|third_party"
+find $SEARCH_PATH -type f -name "*.js" $(printf "! -path *%s* " $(cat $TMP_FILE)) | grep -vE "override|third_party|fsm_libs"
 
 rm $TMP_FILE
